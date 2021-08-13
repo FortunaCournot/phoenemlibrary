f16effa8637b50e58d5fabb28f35166b5ebb3d08
fortunacournot <fortunacournot@gmail.com>
2021-08-13 10:54:12 +0200

    wav trim fixed

--- 2_ssml2phone/mkwav/mkwav.sh
+++ 2_ssml2phone/mkwav/mkwav.sh
@@ -12,8 +12,15 @@ TARGETDIR=../result/ue4phones/$LOCALE/$VOICE/$VOICEMODE
 mkdir -p $TARGETDIR
 rm -f $TARGETDIR/*.wav
 
+THRESHOLD=-54dB
+
+echo "METADATA" $TARGETDIR >$TARGETDIR/metadata.txt
 for p in $PHONES
 do
-    ffmpeg -hide_banner -i $SOURCEDIR/$p".mp3" -af "silenceremove=start_periods=0:start_duration=0.05:start_threshold=-60dB:detection=peak,aformat=dblp,areverse,silenceremove=start_periods=0:start_duration=0.05:start_threshold=-60dB:detection=peak,aformat=dblp,areverse" $TARGETDIR/$p".wav"
+    ffmpeg -hide_banner -i $SOURCEDIR/$p".mp3" -af "silenceremove=start_periods=1:start_duration=0:start_threshold="$THRESHOLD":detection=peak,aformat=dblp,areverse,silenceremove=start_periods=1:start_duration=0:start_threshold="$THRESHOLD":detection=peak,aformat=dblp,areverse" $TARGETDIR/$p".wav"
+    echo $p >>$TARGETDIR/metadata.txt
+    ffmpeg -hide_banner -i $SOURCEDIR/$p".mp3" 2>&1 | grep Duration| sed 's/.*Duration: \(.*\), start/\1/g' >>$TARGETDIR/metadata.txt
+    ffmpeg -hide_banner -i $TARGETDIR/$p".wav" 2>&1 | grep Duration| sed 's/.*Duration: \(.*\), start/\1/g' >>$TARGETDIR/metadata.txt
 done
+cat $TARGETDIR/metadata.txt
 
