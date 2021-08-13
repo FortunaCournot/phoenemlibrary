a11375fe2ef6494fd9c99e8bc685e1c7f7636530
fortunacournot <fortunacournot@gmail.com>
2021-08-13 09:26:11 +0200

    wav generation

--- 2_ssml2phone/mkwav/mkwav.sh
+++ 2_ssml2phone/mkwav/mkwav.sh
@@ -5,8 +5,15 @@ VOICENAME=joanna
 VOICESTYLE=s
 VOICE=$VOICENAME"_"$VOICESTYLE
 VOICEMODE=default
+
+SOURCEDIR=../input/polly_phones/$LOCALE/$VOICE/$VOICEMODE
+TARGETDIR=../result/ue4phones/$LOCALE/$VOICE/$VOICEMODE
+
+mkdir -p $TARGETDIR
+rm -f $TARGETDIR/*.wav
+
 for p in $PHONES
 do
-  ls -l ../input/polly_phones/$LOCALE/$VOICE/$VOICEMODE/$p".mp3"
+    ffmpeg -hide_banner -i $SOURCEDIR/$p".mp3" -af "silenceremove=start_periods=0:start_duration=0.05:start_threshold=-60dB:detection=peak,aformat=dblp,areverse,silenceremove=start_periods=0:start_duration=0.05:start_threshold=-60dB:detection=peak,aformat=dblp,areverse" $TARGETDIR/$p".wav"
 done
 
